3829e3c626eda13445e53f88170c94f15d2e538b
maarwabaalouch <44491909+maarwabaalouch@users.noreply.github.com>
2018-10-26 09:16:42 +0200

    SiteWeb

---
+++ exercice10/mail.php
@@ -0,0 +1,19 @@
+﻿<?php
+  $mail = $_POST['mail'];
+  $pseudo = $_POST['pseudo'];
+  $pass = $_POST['pass'];
+  $titre = "Formulaire d'inscription mon eFormation";
+  $texte = "Voici les donn&eacute;es du formulaire :<br><ul><li>ch1=".$mail."</li><li>ch2=".$pseudo."</li><li>ch3=".$pass."</li></ul>";
+  $destinataire="admin@mediaforma.com";
+  $delapartde = $mail; 
+  $from  = "From:".$delapartde."\n"; 
+  $from .= "MIME-version: 1.0\n";
+  $from .= "Content-type: text/html\n";
+  $from .= "charset: UTF-8\n";
+  if (!mail($destinataire,$titre,$texte,$from))
+    echo "Un problème s'est produit lors de l'envoi du message. Recommencez SVP.";
+  else {
+    echo "Les données ont été transmises.<br><br>";
+    echo "<a href='http://www.mediaforma.com/eformation/exercice7/index.htm'>Retourner au site</a>";
+  }	
+?>
---
+++ exercice10/mail2.php
@@ -0,0 +1,18 @@
+﻿<?php
+  $mail = $_POST['mail'];
+  $message = $_POST['message'];
+  $titre = "Formulaire de contact mon eFormation";
+  $texte = "Voici le message de l'utilisateur : ".htmlentities($message, ENT_QUOTES, "UTF-8");
+  $destinataire="admin@mediaforma.com";
+  $delapartde = $mail; 
+  $from  = "From:".$delapartde."\n"; 
+  $from .= "MIME-version: 1.0\n";
+  $from .= "Content-type: text/html\n";
+  $from .= "charset: UTF-8\n";
+  if (!mail($destinataire,$titre,$texte,$from))
+    echo "Un problème s'est produit lors de l'envoi du message. Recommencez SVP.";
+  else {
+    echo "Les données ont été transmises.<br><br>";
+    echo "<a href='http://www.mediaforma.com/eformation/exercice7/index.htm'>Retourner au site</a>";
+  }	
+?>
